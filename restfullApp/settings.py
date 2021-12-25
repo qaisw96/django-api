3867268f9b5690d3e91bf6cb33b9759b0014bbca
qaisw14 <qaisw14@gmail.com>
2021-12-26 00:18:37 +0200

    procFile11

--- restfullApp/settings.py
+++ restfullApp/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
