819984944455ad5971a2649e701bfb42bff8e4f1
qaisw14 <qaisw14@gmail.com>
2021-12-26 00:29:34 +0200

    procFile16

--- restfullApp/settings.py
+++ restfullApp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a&z__%5#2--%3m03#su7y&g8)3-1%07-6wfhv^3#0!-n#o+*71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
