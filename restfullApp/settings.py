1146ed0974089c721041208ad447492c9ce89d07
qaisw14 <qaisw14@gmail.com>
2021-12-26 12:27:16 +0200

    static root

--- restfullApp/settings.py
+++ restfullApp/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(STATIC_ROOT, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
