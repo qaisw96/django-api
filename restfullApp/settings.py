aa8c495902bfb61952034c6770ed323daa600683
qaisw14 <qaisw14@gmail.com>
2021-12-26 20:56:44 +0200

    cors fix 21

--- restfullApp/settings.py
+++ restfullApp/settings.py
@@ -28,24 +28,6 @@ SECRET_KEY = 'django-insecure-a&z__%5#2--%3m03#su7y&g8)3-1%07-6wfhv^3#0!-n#o+*71
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
-CORS_ALLOWED_ORIGINS = [
-    "https://upbeat-heisenberg-c6d15f.netlify.app",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-   "https://upbeat-heisenberg-c6d15f.netlify.app",
-]
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
 
 # Application definition
 
@@ -76,6 +58,25 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'restfullApp.urls'
+
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGINS = [
+    "https://upbeat-heisenberg-c6d15f.netlify.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+   "https://upbeat-heisenberg-c6d15f.netlify.app",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
 
 TEMPLATES = [
     {
