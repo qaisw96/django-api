50780ffcb3fa911f7af851f34f81c319ec0f0352
qaisw14 <qaisw14@gmail.com>
2021-12-26 19:56:49 +0200

    cors fix 9

--- restfullApp/settings.py
+++ restfullApp/settings.py
@@ -153,4 +153,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+    'https://herokudjango6996.herokuapp.com',
+]
